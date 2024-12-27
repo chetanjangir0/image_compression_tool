4f0fa2466f0277bac133cfb8e5a750239edd0569
Chetan <chetanrjj2002@gmail.com>
2024-12-27 13:36:17 +0530

    added a function to read BMP files

--- main.c
+++ main.c
@@ -24,5 +24,61 @@ typedef struct{
     int biYPelsPerMeter;        // vertical resolution
     unsigned int biClrUsed;     // Number of colors in the color table
     unsigned int biClrImportant;// Important color count
-}BMPDIBHeader;
+}BMPInfoHeader;
 
+
+// RGB pixel structure
+typedef struct{
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+} RGB;
+
+RGB** readBMP(const char* filename, int* width, int* height){
+    FILE* file = fopen(filename, 'rb'); // read mode with binary mode instead of text mode
+    if (!file){
+        printf("could not open file\n");
+        return NULL;
+    }
+
+    // read file header
+    BMPFileHeader fileHeader;
+    fread(&fileHeader, sizeof(BMPFileHeader), 1, file);
+
+    // verify if it's a BMP file
+    if (fileHeader.bfType != 0x4D42){ // BM in hex
+        printf("Not a BMP file\n");
+        fclose(file);
+        return NULL;
+    }
+
+    // read info header
+    BMPInfoHeader infoHeader;
+    fread(&infoHeader, sizeof(BMPInfoHeader), 1, file);
+
+    *width = infoHeader.bitWidth;
+    *height = infoHeader.bitHeight;
+
+    // allocate memory for pixel data
+    RGB** pixels = malloc(*height * sizeof(RGB*));
+    for (int i = 0; i < *height; i++){
+        pixels[i] = malloc(*width * sizeof(RGB));
+    }
+
+    int rowPadding = (4 - ((*width * sizeof(RGB)) % 4)) % 4;
+
+    // move the file pointer to starting of pixel data
+    fseek(file, fileHeader.bf0ffBits, SEEK_SET);
+
+    // read pixel data
+    // bmp stores pixels from bottom to top, left to right
+    for(int i = *height - 1; i >=0; i--){
+        fread(pixels[i], sizeof(RGB), *width, file);
+
+        // skip padding bytes 
+        fseek(file, rowPadding, SEEK_CUR);
+    }
+
+    fclose(file);
+    return pixels;
+}
