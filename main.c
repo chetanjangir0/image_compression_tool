5c7c7bfe7fde6d9462387a7e48a37b312ecf4bf6
Chetan <chetanrjj2002@gmail.com>
2024-12-27 13:41:39 +0530

    added a function to free file memory/ pixels memory

--- main.c
+++ main.c
@@ -82,3 +82,11 @@ RGB** readBMP(const char* filename, int* width, int* height){
     fclose(file);
     return pixels;
 }
+
+void freeBMP(RGB **pixels, int height){
+    for (int i = 0; i < height; i++){
+        free(pixels[i]);
+    }
+    free(pixels);
+}
+
