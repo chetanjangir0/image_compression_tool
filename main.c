7662bd7831e10be8cf314b3706a77011b1aa1f35
Chetan <chetanrjj2002@gmail.com>
2025-01-06 18:24:31 +0530

    added decode rle function

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 #include <string.h>  // for strerror
 #include <errno.h>   // for errno
 // first header Bitmap file header
@@ -45,7 +45,7 @@ typedef struct{
 typedef struct {
     int width;
     int height;
-    RLEPair *Red;
+    RLEPair *red;
     RLEPair *green;
     RLEPair *blue;
     int redSize;
@@ -55,25 +55,45 @@ typedef struct {
 
 RGB** readBMP(const char* filename, int* width, int* height);
 void freeBMP(RGB **pixels, int height);
+void freeRLEData(RLEData* rleData);
+RLEPair* encodeChannel(unsigned char* channelData, int size, int* outSize);
+RLEData* encodeRLE(RGB** pixels, int width, int height);
+RGB** decodeRLE(RLEData* rleData);
 
 
 int main(){
     int width, height;
     const char* filename = "sample_640x426.bmp";
 
-    RGB** pixels = readBMP(filename, &width, &height);
-    if (pixels == NULL){
+    // Read original image
+    RGB** original = readBMP(filename, &width, &height);
+    if (!original) {
+        printf("Test 1 failed: Could not read BMP file\n");
         return 1;
     }
-
-    printf("First pixel RGB: (%d, %d, %d)\n",
-            pixels[0][1].red,
-            pixels[0][1].green,
-            pixels[0][1].blue 
-    );
-
-    freeBMP(pixels, height);
-
+    
+    // Compress
+    RLEData* compressed = encodeRLE(original, width, height);
+    if (!compressed) {
+        printf("Test 1 failed: Compression failed\n");
+        freeBMP(original, height);
+        return 1;
+    }
+    
+    // Decompress
+    RGB** decoded = decodeRLE(compressed);
+    if (!decoded) {
+        printf("Test 1 failed: Decompression failed\n");
+        freeBMP(original, height);
+        // Add freeRLEData when implemented
+        return 1;
+    }
+    
+    // Cleanup
+    freeBMP(original, height);
+    freeBMP(decoded, height);
+    freeRLEData(compressed);
+    return 0;
 }
 
 
@@ -133,6 +153,14 @@ void freeBMP(RGB **pixels, int height){
     }
     free(pixels);
 }
+void freeRLEData(RLEData* rleData) {
+    if (rleData) {
+        free(rleData->red);
+        free(rleData->green);
+        free(rleData->blue);
+        free(rleData);
+    }
+}
 
 RLEPair* encodeChannel(unsigned char* channelData, int size, int* outSize){
     RLEPair* encoded = malloc(size * sizeof(RLEPair)); // worst case size
@@ -142,7 +170,7 @@ RLEPair* encodeChannel(unsigned char* channelData, int size, int* outSize){
     unsigned char currentValue = channelData[0];
 
     for (int i =  0; i < size; i++){
-        if(currentValue == channelData[i] && currentValue < 255){
+        if(currentValue == channelData[i] && currentValue < 256){
             count ++;
         } else {
             encoded[encodeIndex].count = count;
@@ -164,7 +192,7 @@ RLEPair* encodeChannel(unsigned char* channelData, int size, int* outSize){
 }
 
 RLEData* encodeRLE(RGB** pixels, int width, int height){
-    RLEData* rleData = malloc(sizeof(rleData));
+    RLEData* rleData = malloc(sizeof(RLEData));
     rleData->width = width;// stores original width
     rleData->height = height;// stores original height
 
@@ -187,7 +215,7 @@ RLEData* encodeRLE(RGB** pixels, int width, int height){
     }
 
     // Encode each channel
-    rleData->Red = encodeChannel(redChannel, size, &rleData->redSize); 
+    rleData->red = encodeChannel(redChannel, size, &rleData->redSize); 
     rleData->green = encodeChannel(greenChannel, size, &rleData->greenSize); 
     rleData->blue = encodeChannel(blueChannel, size, &rleData->blueSize); 
 
@@ -196,13 +224,56 @@ RLEData* encodeRLE(RGB** pixels, int width, int height){
     free(greenChannel);
     free(blueChannel);
 
-    redChannel = NULL;
-    greenChannel = NULL;
-    blueChannel = NULL;
-
     return rleData;
 }
 
-// RGB** decodeRLE(RLEData* RLEData){
-//     RGB** pixels
-// }
+RGB** decodeRLE(RLEData* rleData){
+    int width = rleData->width;
+    int height = rleData->height;
+    RGB** pixels = malloc(height * sizeof(RGB*));
+
+    for(int i = 0; i < height; i++){
+        pixels[i] = malloc(width * sizeof(RGB));
+    }
+
+    int blueIndex = 0;
+    for(int i = 0; i < rleData->blueSize; i++){
+        unsigned char cnt = rleData->blue[i].count;
+        unsigned char val = rleData->blue[i].value;
+
+        for(int j = 0; j < cnt; j++){
+            int row = blueIndex / width;
+            int col = blueIndex % width;
+            pixels[row][col].blue = val;
+            blueIndex++;
+        }
+    }
+
+    int redIndex = 0;
+    for(int i = 0; i < rleData->redSize; i++){
+        unsigned char cnt = rleData->red[i].count;
+        unsigned char val = rleData->red[i].value;
+
+        for(int j = 0; j < cnt; j++){
+            int row = redIndex / width;
+            int col = redIndex % width;
+            pixels[row][col].red = val;
+            redIndex++;
+        }
+    }
+
+    int greenIndex = 0;
+    for(int i = 0; i < rleData->greenSize; i++){
+        unsigned char cnt = rleData->green[i].count;
+        unsigned char val = rleData->green[i].value;
+
+        for(int j = 0; j < cnt; j++){
+            int row = greenIndex / width;
+            int col = greenIndex % width;
+            pixels[row][col].green = val;
+            greenIndex++;
+        }
+    }
+
+    return pixels;
+}
